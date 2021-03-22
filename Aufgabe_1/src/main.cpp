cf399d7d24c51987760615b4c0e09f9315fb788d
LucRome <64947931+LucRome@users.noreply.github.com>
2021-03-22 23:14:43 +0100

    content for main()

--- Aufgabe_1/src/main.cpp
+++ Aufgabe_1/src/main.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#include "OptParser.h"
+#include <string>
 
 int main()
 {
-    std::cout << "hello";
+    //Parameter list
+    char arg0[] = "ProgName";
+    char arg1[] = "-x";
+    char arg2[] = "-y=as";
+    char arg3[] = "-k";
+
+    char* argv[] = {arg0, arg1, arg2, arg3};
+
+    int argc = sizeof(argv)/sizeof(argv[0]);
+
+    CmdLineOptParser cmdP = CmdLineOptParser();
+    cmdP.Parse(argc, argv);
 }
