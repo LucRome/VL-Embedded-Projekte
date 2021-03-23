4d2d9aec7decf665a06fba67ba44d414d1d983ab
LucRome <64947931+LucRome@users.noreply.github.com>
2021-03-23 21:22:08 +0100

    implement Parse()

--- Aufgabe_1/src/OptParser.cpp
+++ Aufgabe_1/src/OptParser.cpp
@@ -1,8 +1,54 @@
 #include "OptParser.h"
+#include "OptChecker.h"
 #include <stdio.h>
 
 bool CmdLineOptParser::Parse(int argc, char* argv[]) {
-    
+
+    //iterate over all arguments and check them
+    //only while result is not false
+    for(int i = 0; i < argc; i++) {
+        char varName = 0;
+        char* ptr_value = nullptr;
+        
+        //must start with Variable
+        if(argv[i] == nullptr || !CmdLineOptChecker::isVariable(argv[i])) {
+            return false;
+        } else {
+            //+ 1: '-'x (argv[i] points to '-' and x is needed)
+            int offset = 1;
+            varName = *(argv[i] + offset);
+            
+            // get value
+            offset = 2;
+            if(!CmdLineOptChecker::EOA(argv[i] + offset)) {
+                //-> Value or Equals
+                
+                if(CmdLineOptChecker::isEquals(argv[i] + offset)) {
+                    offset++; //ignore Equals
+                }
+
+                if(!CmdLineOptChecker::isValue(argv[i] + offset)) {
+                    return false;
+                } else {
+                    ptr_value = (argv[i] + offset);
+                }
+
+            } else {
+                //check next argument
+                i++;
+                if(argv[i] == nullptr || !CmdLineOptChecker::isValue(argv[i])) {
+                    return false;
+                } else {
+                    ptr_value = (argv[i]);
+                }
+            }
+        }
+        //ptr_value should never be nullptr here (= error)
+        bool optionSuccess = Option(varName, ptr_value);
+        if(!optionSuccess) {
+            return false;
+        }
+    }
 }
 
 bool CmdLineOptParser::Option(const char c, const char* info) {
