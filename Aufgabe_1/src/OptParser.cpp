e4dbb210e97f84179e3ef501768e7271411e912a
LucRome <64947931+LucRome@users.noreply.github.com>
2021-03-23 11:02:32 +0100

    start to implement CMDLineOptParser

---
+++ Aufgabe_1/src/OptParser.cpp
@@ -0,0 +1,22 @@
+#include "OptParser.h"
+
+bool CmdLineOptParser::Parse(int argc, char* argv[]) {
+    //State machine to Parse input
+    int state = 0;
+    
+    switch (state)
+    {
+    case 0:
+        
+        break;
+    
+    default:
+        break;
+    }
+
+    return true;    
+}
+
+bool CmdLineOptParser::Option(char c, const char* info) {
+    return true;
+}
