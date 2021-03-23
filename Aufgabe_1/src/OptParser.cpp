5ba53aa7161ceea395838319d8f9978790e9e28e
LucRome <64947931+LucRome@users.noreply.github.com>
2021-03-23 21:20:16 +0100

    implement Option() for Test-purposes

--- Aufgabe_1/src/OptParser.cpp
+++ Aufgabe_1/src/OptParser.cpp
@@ -1,5 +1,16 @@
 #include "OptParser.h"
+#include <stdio.h>
 
 bool CmdLineOptParser::Parse(int argc, char* argv[]) {
     
+}
+
+bool CmdLineOptParser::Option(const char c, const char* info) {
+    //For Debugging: just print the content
+    if(info == nullptr) {
+        // no value
+        printf("Variable: %c = TRUE");
+    } else {
+        printf("Variable: %c = %s", c, info);
+    }
 }
