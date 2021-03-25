5b47d1b74097483e61eea170b948d21f250623ec
LucRome <64947931+LucRome@users.noreply.github.com>
2021-03-25 09:24:52 +0100

    UnitTests for OptChecker

--- Aufgabe_1/unittest/tests.cpp
+++ Aufgabe_1/unittest/tests.cpp
@@ -2,7 +2,11 @@
 
 #include "OptChecker.h"
 
-TEST_CASE("OptChecker: isVariable(..)", "[OptChecker]") {
+//////////////////////////////////////////////////////////
+//          CmdLineOptChecker
+//////////////////////////////////////////////////////////
+
+TEST_CASE("OptChecker::isVariable(..)", "[OptChecker]") {
     SECTION("Correct:") {
         char str1[] = "-a";
         char str2[] = "-z";
@@ -25,4 +29,56 @@ TEST_CASE("OptChecker: isVariable(..)", "[OptChecker]") {
     }
 }
 
-TEST_CASE("OptChecker: is")
+TEST_CASE("OptChecker::isValue(..)", "[OptChecker]") {
+    SECTION("Correct:") {
+        char str1[] = "hel0";
+        char str2[] = "023ka";
+
+        REQUIRE(CmdLineOptChecker::isValue(str1));
+        REQUIRE(CmdLineOptChecker::isValue(str2));
+    }
+    SECTION("Incorrect:") {
+        char str1[] = "-as";
+        char str2[] = "as vde";
+        char str3[] = "l_";
+
+        REQUIRE_FALSE(CmdLineOptChecker::isValue(str1));
+        REQUIRE_FALSE(CmdLineOptChecker::isValue(str2));
+        REQUIRE_FALSE(CmdLineOptChecker::isValue(str3));
+    }
+}
+
+TEST_CASE("OptChecker::isEquals(..)", "[OptChecker]") {
+    SECTION("Correct:") {
+        char str1[] = "=";
+        char str2[] = " ";
+
+        REQUIRE(CmdLineOptChecker::isEquals(str1));
+        REQUIRE(CmdLineOptChecker::isEquals(str2));
+    }
+    SECTION("Incorrect:") {
+        char str1[] = "";
+        char str2[] = "x";
+
+        REQUIRE_FALSE(CmdLineOptChecker::isEquals(str1));
+        REQUIRE_FALSE(CmdLineOptChecker::isEquals(str2));
+    }
+}
+
+TEST_CASE("OptChecker::EOA(..)", "[OptChecker]") {
+    SECTION("Correct:") {
+        char str1[] = "a";
+        char* eoa = str1 + 1;
+        REQUIRE(CmdLineOptChecker::EOA(eoa));
+    }
+    SECTION("Incorrect:") {
+        char str1[] = "1";
+        REQUIRE_FALSE(CmdLineOptChecker::EOA(str1));
+    }
+}
+
+// Maybe also test private fkt. of OptChecker
+
+//////////////////////////////////////////////////////////
+//          CmdLineOptParser
+//////////////////////////////////////////////////////////
