ea4433121b0fe818ef0db514eeff772210c1c345
LucRome <64947931+LucRome@users.noreply.github.com>
2021-04-03 14:47:14 +0200

    basics of Printf function

--- Aufgabe_2/src/Printf.cpp
+++ Aufgabe_2/src/Printf.cpp
@@ -1,7 +1,32 @@
 #include "Printf.h"
+#include "PrintfUtils.h"
+
 #include <stdarg.h>
 
 char* Printf(char* dst, const void* end, const char* fmt, ...) {
     
+    if(!dst || !end || !fmt) {
+        return nullptr;
+    }
+    else {
+        int argc_expected = PrintfUtils::getNeededNrOfArguments(fmt);
+    
+        if(argc_expected != -1) { // valid
+            //access "..."
+            // va_list params;
+            // va_start(params, argc_expected);
+
+            while(!PrintfUtils::EOS(fmt)) {
+                if(PrintfUtils::isSpecifier(fmt)) {
+                    //TODO process specifiers
+                } 
+                else {
+                    //TODO process chars
+
+                }
+            }
+        }
+    }
+
     return nullptr;
 }
