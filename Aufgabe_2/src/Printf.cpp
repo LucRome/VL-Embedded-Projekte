1f426385277c10fda90213ee3a5de5723d594d7d
LucRome <64947931+LucRome@users.noreply.github.com>
2021-04-03 20:09:03 +0200

    implement Printf to process "%c"

--- Aufgabe_2/src/Printf.cpp
+++ Aufgabe_2/src/Printf.cpp
@@ -1,29 +1,66 @@
 #include "Printf.h"
 #include "PrintfUtils.h"
+#include "DestAppender.h"
 
 #include <stdarg.h>
 
+namespace {
+    using namespace::PrintfUtils;
+
+    bool processSpecifier(va_list& params, const PrintfUtils::SpecifierType specType, DestAppender& dstApp) {
+        
+        switch (specType)
+            {
+            case SpecifierType::Binary:
+                break;
+            case SpecifierType::Char: {
+                int i = va_arg(params, int);
+                dstApp.processChar(&i);
+                break;
+            }
+            case SpecifierType::Hexa:
+                break;
+            case SpecifierType::None:
+                break;
+            case SpecifierType::SignedInt:
+                break;
+            case SpecifierType::SkipFormat:
+                break;
+            case SpecifierType::String:
+                break;
+            case SpecifierType::UnsignedInt:
+                break;
+            default:
+                break;
+            }
+        return true;
+    }
+}
+
+using namespace PrintfUtils;
+
 char* Printf(char* dst, const void* end, const char* fmt, ...) {
     
+    DestAppender destAppender = DestAppender(dst, end);
+
     if(!dst || !end || !fmt) {
         return nullptr;
     }
     else {
-        int argc_expected = PrintfUtils::getNeededNrOfArguments(fmt);
-    
-        if(argc_expected != -1) { // valid
-            //access "..."
-            // va_list params;
-            // va_start(params, argc_expected);
+        bool success = true;
+        //access "..."
+        va_list params;
+        va_start(params, fmt);
 
-            while(!PrintfUtils::EOS(fmt)) {
-                if(PrintfUtils::isSpecifier(fmt)) {
-                    //TODO process specifiers
-                } 
-                else {
-                    //TODO process chars
-
-                }
+        while(!PrintfUtils::EOS(fmt) && success) {
+            if(PrintfUtils::isSpecifier(fmt)) {
+                processSpecifier(params, PrintfUtils::getSpecifierType(fmt), destAppender);
+                
+                fmt += 2;
+            } 
+            else {
+                destAppender.appendCharToDest(*fmt);
+                fmt++;
             }
         }
     }
