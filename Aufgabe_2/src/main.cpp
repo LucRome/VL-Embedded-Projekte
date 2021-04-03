1e71da7263d35e5fa261cd7313460a25e0274506
LucRome <64947931+LucRome@users.noreply.github.com>
2021-04-03 14:48:03 +0200

    test DestAppender in main

--- Aufgabe_2/src/main.cpp
+++ Aufgabe_2/src/main.cpp
@@ -1,11 +1,27 @@
 #include "Printf.h"
 #include <stdio.h>
+#include "DestAppender.h"
 
 int main() {
-    char* ret = Printf(nullptr, nullptr, nullptr, 'a');
-    if(!ret) {
-        printf("null");
-    } else {
-        printf(ret);
-    }
+    {
+        //Allocate dst
+        const int dst_length = 6;
+        char dst[dst_length] = "abcde";
+        printf("%c \n", dst[0]);
+        DestAppender dstAppender = DestAppender(dst, dst + dst_length - 1);
+        printf("%s\n", dst);
+        char i = '0';
+        while(dstAppender.appendCharToDest(i)) {
+            printf("dst %c: %s\n", i, dst);
+            i++;
+        }
+        dstAppender.resetDst();
+    }  
+
+    // char* ret = Printf(nullptr, nullptr, nullptr, 'a');
+    // if(!ret) {
+    //     printf("null");
+    // } else {
+    //     printf(ret);
+    // }
 }
